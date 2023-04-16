ec341652dce60f964971f099a1a06e08ddcb4b02
Hicret Kuş <hicret.kus@hotmail.com>
2023-04-16 21:00:10 +0300

    dto

---
+++ Northwind.Application/Dtos/CustomerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Application.Dtos
+{
+	public class CustomerDto
+	{
+		public string CustomerID { get; set; }
+		public string CustomerName { get; set; }
+		public string ContactName { get; set; }
+	}
+}
--- Northwind.Infrastructure/Persistence/NorthwindDbContext.cs
+++ Northwind.Infrastructure/Persistence/NorthwindDbContext.cs
@@ -26,83 +26,83 @@ namespace Northwind.Infrastructure.Persistence
 		public DbSet<Employee> Employees { get; set; }
 		public DbSet<Product> Products { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.Entity<Customer>().HasKey(x => x.CustomerID);
-			modelBuilder.Entity<Employee>().HasKey(x => x.EmployeeID);
-			modelBuilder.Entity<Product>().HasKey(x => x.ProductID);
-		}
-
-
-
 		//protected override void OnModelCreating(ModelBuilder modelBuilder)
 		//{
-
-		//	modelBuilder.haskey
-		//modelBuilder.ApplyConfiguration(new CustomerMapping());
-		//modelBuilder.ApplyConfiguration(new EmployeeMapping());
-		//modelBuilder.ApplyConfiguration(new ProductMapping());
-
-		//base.OnModelCreating(modelBuilder);
-		//}
-		//public class CustomerMapping : IEntityTypeConfiguration<Customer>
-		//{
-		//	public void Configure(EntityTypeBuilder<Customer> builder)
-		//	{
-		//		builder.HasKey(x => x.CustomerID);
-		//		builder.Property(x => x.CustomerID).HasMaxLength(5).IsRequired();
-		//		builder.Property(x => x.CustomerName).HasMaxLength(40).IsRequired();
-		//		builder.Property(x => x.ContactTitle).HasMaxLength(30);
-		//		builder.Property(x => x.Address).HasMaxLength(60);
-		//		builder.Property(x => x.City).HasMaxLength(15);
-		//		builder.Property(x => x.Region).HasMaxLength(15);
-		//		builder.Property(x => x.PostalCode).HasMaxLength(10);
-		//		builder.Property(x => x.Country).HasMaxLength(15);
-		//		builder.Property(x => x.Phone).HasMaxLength(24);
-		//		builder.Property(x => x.Fax).HasMaxLength(24);
-		//	}
+		//	modelBuilder.Entity<Customer>().HasKey(x => x.CustomerID);
+		//	modelBuilder.Entity<Employee>().HasKey(x => x.EmployeeID);
+		//	modelBuilder.Entity<Product>().HasKey(x => x.ProductID);
 		//}
 
-		//public class EmployeeMapping : IEntityTypeConfiguration<Employee>
-		//{
-		//	public void Configure(EntityTypeBuilder<Employee> builder)
-		//	{
-		//		builder.HasKey(x => x.EmployeeID);
-		//		builder.Property(x => x.LastName).HasMaxLength(20).IsRequired();
-		//		builder.Property(x => x.FirstName).HasMaxLength(10).IsRequired();
-		//		builder.Property(x => x.Title).HasMaxLength(30);
-		//		builder.Property(x => x.TitleOfCourtesy).HasMaxLength(25);
-		//		builder.Property(x => x.Address).HasMaxLength(60);
-		//		builder.Property(x => x.City).HasMaxLength(15);
-		//		builder.Property(x => x.Region).HasMaxLength(15);
-		//		builder.Property(x => x.PostalCode).HasMaxLength(10);
-		//		builder.Property(x => x.Country).HasMaxLength(15);
-		//		builder.Property(x => x.HomePhone).HasMaxLength(24);
-		//		builder.Property(x => x.Extension).HasMaxLength(4);
-		//		builder.Property(x => x.Photo).HasMaxLength(15);
-		//		builder.Property(x => x.Notes).HasColumnType("ntext");
-
-		//		builder.Property(x => x.PhotoPath).HasMaxLength(255);
-		//	}
-		//}
-
-		//public class ProductMapping : IEntityTypeConfiguration<Product>
-		//{
-		//	public void Configure(EntityTypeBuilder<Product> builder)
-		//	{
-		//		builder.HasKey(x => x.ProductID);
-		//		builder.Property(x => x.ProductID).IsRequired();
-		//		builder.Property(x => x.ProductName).HasMaxLength(40).IsRequired();
-		//		builder.Property(x => x.QuantityPerUnit).HasMaxLength(20);
-		//		builder.Property(x => x.UnitPrice).HasColumnType("money");
-		//		builder.Property(x => x.UnitsInStock).HasColumnType("smallint");
-		//		builder.Property(x => x.UnitsOnOrder).HasColumnType("smallint");
-		//		builder.Property(x => x.ReorderLevel).HasColumnType("smallint");
 
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
 
-		//	}
-		//}
+			
+		modelBuilder.ApplyConfiguration(new CustomerMapping());
+			modelBuilder.ApplyConfiguration(new EmployeeMapping());
+			modelBuilder.ApplyConfiguration(new ProductMapping());
+
+			base.OnModelCreating(modelBuilder);
+		}
+		public class CustomerMapping : IEntityTypeConfiguration<Customer>
+		{
+			public void Configure(EntityTypeBuilder<Customer> builder)
+			{
+				builder.HasKey(x => x.CustomerID);
+				builder.Property(x => x.CustomerID).HasMaxLength(5).IsRequired();
+				builder.Property(x => x.CustomerName).HasMaxLength(40).IsRequired();
+				builder.Property(x => x.ContactTitle).HasMaxLength(30);
+				builder.Property(x => x.Address).HasMaxLength(60);
+				builder.Property(x => x.City).HasMaxLength(15);
+				builder.Property(x => x.Region).HasMaxLength(15);
+				builder.Property(x => x.PostalCode).HasMaxLength(10);
+				builder.Property(x => x.Country).HasMaxLength(15);
+				builder.Property(x => x.Phone).HasMaxLength(24);
+				builder.Property(x => x.Fax).HasMaxLength(24);
+			}
+		}
+
+		public class EmployeeMapping : IEntityTypeConfiguration<Employee>
+		{
+			public void Configure(EntityTypeBuilder<Employee> builder)
+			{
+				builder.HasKey(x => x.EmployeeID);
+				builder.Property(x => x.LastName).HasMaxLength(20).IsRequired();
+				builder.Property(x => x.FirstName).HasMaxLength(10).IsRequired();
+				builder.Property(x => x.Title).HasMaxLength(30);
+				builder.Property(x => x.TitleOfCourtesy).HasMaxLength(25);
+				builder.Property(x => x.Address).HasMaxLength(60);
+				builder.Property(x => x.City).HasMaxLength(15);
+				builder.Property(x => x.Region).HasMaxLength(15);
+				builder.Property(x => x.PostalCode).HasMaxLength(10);
+				builder.Property(x => x.Country).HasMaxLength(15);
+				builder.Property(x => x.HomePhone).HasMaxLength(24);
+				builder.Property(x => x.Extension).HasMaxLength(4);
+				builder.Property(x => x.Photo).HasMaxLength(15);
+				builder.Property(x => x.Notes).HasColumnType("ntext");
+
+				builder.Property(x => x.PhotoPath).HasMaxLength(255);
+			}
+		}
+
+		public class ProductMapping : IEntityTypeConfiguration<Product>
+		{
+			public void Configure(EntityTypeBuilder<Product> builder)
+			{
+				builder.HasKey(x => x.ProductID);
+				builder.Property(x => x.ProductID).IsRequired();
+				builder.Property(x => x.ProductName).HasMaxLength(40).IsRequired();
+				builder.Property(x => x.QuantityPerUnit).HasMaxLength(20);
+				builder.Property(x => x.UnitPrice).HasColumnType("money");
+				builder.Property(x => x.UnitsInStock).HasColumnType("smallint");
+				builder.Property(x => x.UnitsOnOrder).HasColumnType("smallint");
+				builder.Property(x => x.ReorderLevel).HasColumnType("smallint");
+
+
+
+			}
+		}
 	}
 
 
